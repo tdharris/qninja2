a34369c69a4c68e334d61db4b697b6ca0d2efb62
Tyler Harris <tylerdavidharris@gmail.com>
2015-10-09 22:21:42 -0600

    cleanup

--- server/lib/qmanagement/js/finishTask/index.js
+++ server/lib/qmanagement/js/finishTask/index.js
@@ -7,17 +7,13 @@ module.exports = function(task) {
     return function(err, results) {
         var eventHeader = "[taskHandler] [finishTask]";
         if(err) logme.error(eventHeader, "Problem processing request:", JSON.stringify(err));
-        console.log('-----', 
-            err, 
+        console.log(
             task.report.send
         );
-        async.series([
-            task.report.send(results), // send final report of task to engineer
-            cleanup(task) // close smtp-pool transport
-        ], function(err){
-            logme.info(eventHeader, "Successfully processed request");
 
-        });  
+        // send final report of task to engineer & close smtp-pool transport
+        task.report.send(results, cleanup(task));
+        
     };
 
 };
